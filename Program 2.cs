9624a769198d2261bb5ddb5776a3f3844aff885a
TerezaPodhradska <92864146+TerezaPodhradska@users.noreply.github.com>
2022-01-23 02:57:23 +0100

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Magnety
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ZAPSÁNÍ MAGNETŮ DO POLE
+
+            Console.WriteLine("Kolik máme magnetů: ");
+            int pocetMagnetu = int.Parse(Console.ReadLine());
+
+            string[] pole = new string[pocetMagnetu];
+            
+            for (int i = 0; i < pole.Length; i++)
+            {
+                Console.WriteLine("Položte magnet");
+                pole[i] = Console.ReadLine(); 
+               
+            }
+
+            //ZJIŠTĚNÍ DÉLKY ŘETĚZCE
+
+            int delkaRetezce = 0;
+            for (int p = 0; p < pole.Length - 1; p++)
+            {
+
+                if (string.Equals(pole[p], pole[p + 1]) == false) {
+
+                    delkaRetezce++;
+
+                 }
+
+            }
+            if(delkaRetezce > 0)
+            {
+                delkaRetezce++;
+            }
+
+
+
+            Console.WriteLine("Řetězec je dlouhý: " + delkaRetezce);
+
+        }
+    }
+}
